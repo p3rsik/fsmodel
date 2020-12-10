8357f67d70db1d28b79b0891d1e3ca4a78d9985b
p3rsik <yroslav541@gmail.com>
2020-12-10 03:47:16 +0200

    Add basic CLI

--- src/CLI.hs
+++ src/CLI.hs
@@ -1,1 +1,25 @@
-module CLI where
+module CLI
+  ( getArgs,
+  )
+where
+
+import Options.Generic
+import System.IO
+import System.Directory
+import System.FilePath
+
+data CMD = CMD
+  { mountDir :: Maybe FilePath <?> "Dir with filesystems(files) to mount, defaults to current dir"
+  }
+  deriving (Generic, Show)
+
+instance ParseRecord CMD
+
+getArgs :: IO CMD
+getArgs = do
+  getRecord "FileSystem model"
+
+-- | Get all FileSystem Model files at 'FilePath'
+getFSFiles :: FilePath -> IO [FilePath]
+getFSFiles path = do
+  filter ((== "fsm") . takeExtension) <$> listDirectory path

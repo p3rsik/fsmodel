5ee41f341c721eaffca990bb9d5bc29cc91b0af0
413x <dedifferentiator@protonmail.ch>
2020-12-14 02:38:07 +0200

    feat: add implementation of the top-level fs functions

--- src/FileSystem.hs
+++ src/FileSystem.hs
@@ -16,56 +16,259 @@ module FileSystem
   )
 where
 
-import Data.Vector hiding (create)
+import Data.Bit
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Vector.Unboxed as V
 import Data.Word
+import Control.Monad.State
 import FileSystem.Internal
 import FileSystem.State
+import System.FilePath.Posix
 import Prelude hiding (read, truncate)
 
 -- | Returns information about file at 'FilePath'
 filestat :: FS m => FilePath -> m FileStat
-filestat = undefined
+filestat path = do
+  FSS { inodes } <- get
+  return case getINodeByPath path inodes of
+    Right i -> fileStat i
+    Left  e -> error $ show e
 
 -- | Creates new file at prefix 'FilePath' with name postfix 'FilePath'
 create :: FS m => FilePath -> m ()
-create = undefined
+create path = do 
+  st@FSS { inodes, inodeBitMap, blockBitMap, mem } <- get
+
+  -- TODO: how to check it smoothly
+  let _ = case getINodeByPath path inodes of
+            Left e  -> e
+            _       -> error $ show EEXIST
+  let index = case bitIndex 0 inodeBitMap of
+            Just i  -> i
+            Nothing -> error $ show ENOSPC
+
+  let ino = inodes !! index
+
+  let (newBlocks, newBMap) = findNFreeBlocks blockBitMap 1
+  let newIndex = fromIntegral $ head newBlocks
+  let newMem = init $ take newIndex mem
+            <> pure (Block $ putName (toVectorWord8 $ BS.pack path)
+                           $ unBlock $ mem !! newIndex)
+            <> drop newIndex mem
+
+
+  let ni = ino { blockCount = 1
+               , fileStat = FS (length path) File
+               , blocks = newBlocks
+               }
+
+  -- TODO: update directory cont*nt with this file
+  put $ st { inodeBitMap = V.modify (`flipBit` index) inodeBitMap
+           , blockBitMap = newBMap
+             -- it shouldn't panic at index 0,
+             -- because 0th index is "/" root dir
+             -- which shouldn't be deleted
+             -- and something created over it
+           , inodes = init $ take index inodes
+                   <> pure ni
+                   <> drop index inodes
+           , mem = newMem
+           }
 
 -- | Opens file at 'FilePath' returning 'FileDescriptor' for future uses
 open :: FS m => FilePath -> m FileDescriptor
-open = undefined
+open path = do
+  st@FSS { inodes, fdlist } <- get
+  let x = case getINodeByPath path inodes of
+            Right INode { blocks } -> FileDescriptor (length fdlist) blocks
+            Left  e -> error $ show e
+  
+  put $ st { fdlist=fdlist <> [x] }
+
+  return x
 
 -- | Closes given 'FileDescriptor'
 close :: FS m => FileDescriptor -> m ()
-close = undefined
+close FileDescriptor { unID } = do
+  st@FSS { fdlist } <- get
+  put $ st { fdlist = filter (\(FileDescriptor fid _ ) -> unID /= fid) fdlist }
 
 -- | Reads 'Int' bytes at 'Int' offset from 'FileDescriptor'
-read :: FS m => FileDescriptor -> Int -> Int -> m (Vector Word8)
-read = undefined
+read :: FS m => FilePath -> FileDescriptor -> Int -> Int -> m (V.Vector Word8)
+read _ FileDescriptor { unBlocks } bytes off =
+  V.take bytes . V.drop off <$> fromIndToVec unBlocks
+
 
 -- | Writes 'Int' bytes at 'Int' offset to 'Filedescriptor'
-write :: FS m => FileDescriptor -> Int -> Int -> Vector Word8 -> m Int
-write = undefined
+write :: FS m => FilePath -> FileDescriptor -> Int -> Int -> V.Vector Word8 -> m Int
+write _ FileDescriptor { unBlocks } bytes off d = do
+  FSS { metadata=SBlock { blockSize }, mem} <- get
 
--- | Creates symbolic link from 'FilePath' to 'FilePath'
+  fdBlocks <- fromIndToVec unBlocks
+  when (off + bytes < V.length fdBlocks) (error $ show ENOMEM)
+
+  let _ = go (fromIntegral blockSize) mem unBlocks (V.take bytes d) (V.length d)
+
+  return bytes
+  where
+    go :: Int
+       -> [Block] -- mem
+       -> [Index Block] -- blocks read from fd
+       -> V.Vector Word8 -- data to write
+       -> Int -- offset for data to write
+       -> [Block] -- new mem
+    go _ mem _ _ 0 = mem
+    go bsize mem (b:bs) v vecOff = do
+      go bsize
+         (init (take (fromIntegral b) mem)
+          <> [Block
+                -- if number of bytes to write is less than
+                -- block size than write them only to
+                -- a part of vector
+                if bsize <= vecOff
+                  then (V.++)
+                       (unBlock (last $ take (fromIntegral b) mem))
+                       (V.take bsize (V.drop (V.length v - vecOff) v))
+                  else V.take bsize $ V.drop (V.length v - vecOff) v]
+          <> drop (fromIntegral b) mem)
+         bs
+         v
+         (if bsize <= vecOff
+            then 0
+            else vecOff - bsize)
+    go _ mem _ _ _ = mem
+
+-- | Creates hard link in 'FilePath' to 'FilePath'
 link :: FS m => FilePath -> FilePath -> m ()
-link = undefined
+link src dst = undefined
 
--- | Destroys symbolic link at 'FilePath'
+-- | Destroys link at 'FilePath'
 unlink :: FS m => FilePath -> m ()
-unlink = undefined
+unlink path = do
+  st@FSS { .. } <- get
+
+  (newInos, newIMap, newBMap) <- unlinkINodeByPath path inodes inodeBitMap blockBitMap
+  put st { inodes=newInos
+         , inodeBitMap=newIMap
+         , blockBitMap=newBMap
+         }
 
 -- | Truncates file at 'FilePath' to 'Int' bytes
 truncate :: FS m => FilePath -> Int -> m ()
-truncate = undefined
+truncate path size = do
+  st@FSS { metadata=SBlock{ blockSize }, inodes, blockBitMap} <- get
+
+  let ino@INode { blockCount } = case getINodeByPath path inodes of
+               Right i -> i
+               Left  e   -> error $ show e
+
+  let index = case getINodeIndexByPath path inodes of
+            Just i -> i
+            Nothing    -> error $ show ENXIST
+
+  let (newBlocks, newBitMap) = findNFreeBlocks
+                               blockBitMap
+                               (size - fromIntegral (blockCount * blockSize))
+
+  -- if size > size of blocks of inodes
+  let newInos = init $ take index inodes
+             <> pure (ino { blocks=newBlocks })
+             <> drop index inodes
+
+  put st { inodes=newInos, blockBitMap=newBitMap }
+
 
 -- | Creates directory at 'FilePath'
 mkdir :: FS m => FilePath -> m ()
-mkdir = undefined
+mkdir path = undefined
 
 -- | Removes directory at 'FilePath'
 rmdir :: FS m => FilePath -> m ()
-rmdir = undefined
+rmdir = unlink
 
--- | Creates soft symbolic link from 'FilePath' to 'FilePath'
+-- | Creates symbolic link from 'FilePath' to 'FilePath'
 symlink :: FS m => FilePath -> FilePath -> m ()
-symlink = undefined
+symlink src dst = do
+  -- try to create file and catch error in this case
+  let v = toVectorWord8 $ BS.pack dst
+  -- changing size of src file to the size of ther dst link
+  truncate src $ V.length v
+
+  fd <- open src 
+  _ <- write src fd (V.length v) 0 v
+  close fd
+
+
+-- | Filters list of inodes and returns the first one which name
+-- matches path
+getINodeByPath :: FilePath -> [INode] -> Either FSError INode
+getINodeByPath "" _ = Left ENXIST
+getINodeByPath path inodes =
+  case filter f inodes of
+    (x:_) -> Right x
+    _     -> Left ENXIST
+  where f = \i -> path == getName (blocks i)
+
+-- | Finds inode index by filename
+getINodeIndexByPath :: FilePath -> [INode] -> Maybe Int
+getINodeIndexByPath path inodes = go inodes path 0
+  where
+    go :: [INode] -> FilePath -> Int -> Maybe Int
+    go (i:is) p acc =
+          if p == getName (blocks i)
+            then Just acc
+            else go is p $ acc + 1
+    go _ _ _ = Nothing
+          
+-- | Unlinks entity from INode
+unlinkINodeByPath :: FS m => FilePath
+                          -> [INode]
+                          -> INodeBitMap
+                          -> BlockBitMap
+                          -> m ([INode], INodeBitMap, BlockBitMap)
+unlinkINodeByPath path inodes inmap ibmap = do
+  -- index of inode to unlink
+  let index = case getINodeIndexByPath path inodes of
+            Just i -> i
+            Nothing -> error $ show ENXIST
+
+  let INode { blocks } = inodes !! index
+  let newBMap = ibmap V.// go blocks []
+          where go :: [Index Block] -> [(Int, Bit)] -> [(Int, Bit)]
+                go (i:is) vec = go is $ vec <> pure (fromIntegral i, Bit False)
+                go _ vec = vec
+
+  let newInos = init (take index inodes)
+             <> pure (INode 0 (FS 0 File) [])
+             <> drop index inodes
+  let newIMap = V.init (V.take index inmap)
+             <> V.singleton (Bit False)
+             <> V.drop index inmap
+
+  return (newInos, newIMap, newBMap)
+
+
+-- | Converts indices to Vector
+fromIndToVec :: FS m => [Index Block] -> m (V.Vector Word8)
+fromIndToVec inds = do
+  FSS { mem } <- get
+  return $ go mem inds V.empty
+
+  where go :: [Block] -> [Index Block] -> V.Vector Word8 -> V.Vector Word8
+        go mem (i:is) acc = go mem is $ acc V.++ unBlock (mem !! fromIntegral i)
+        go _   []     acc = acc
+
+-- | Find N free blocks
+findNFreeBlocks :: BlockBitMap -> Int -> ([Index Block], BlockBitMap)
+findNFreeBlocks blockMap n = go blockMap n (V.length blockMap) []
+  where go :: BlockBitMap -> Int -> Int -> [Index Block] -> ([Index Block], BlockBitMap)
+        go _ _ 0 _ = error $ show ENOSPC
+        go bmap count off acc =
+          case bmap V.! (V.length bmap - off) of
+            Bit False -> if count == 0
+                           then (acc <> pure (fromIntegral $ V.length bmap - off), bmap)
+                           else go (modMap bmap off) (count - 1) (off - 1) acc
+            Bit True  -> go bmap count (off - 1) acc
+
+        modMap bmap off = V.modify (`flipBit` (V.length bmap - off)) bmap 
--- src/FileSystem/Internal.hs
+++ src/FileSystem/Internal.hs
@@ -4,32 +4,40 @@
 --
 -- This module contains types and functions which are used for internal representation of the filesystem
 module FileSystem.Internal
-  ( BlockBitMap,
-    INodeBitMap,
-    SuperBlock (..),
-    INode (..),
-    FileType (..),
-    Block (..),
-    FileStat (..),
-    FSError (..),
-    FileDescriptor (..),
-    File,
+  ( BlockBitMap
+  , INodeBitMap
+  , SuperBlock (..)
+  , INode (..)
+  , Index
+  , FileType (..)
+  , Block (..)
+  , FileStat (..)
+  , FSError (..)
+  , FileDescriptor (..)
+  , readDir
+  , readLink
+  , getName
+  , putName
+  , toByteString
+  , toVectorWord8
   )
 where
 
 import           Data.Bit
-import           Data.Vector
+import qualified Data.ByteString as B
+import qualified Data.Vector.Unboxed as V
 import           Data.Word
+import           Prelude
 
 -- | Bitmap for blocks
 -- 1 indicating that block is used
 -- 0 indicating that block is free
-type BlockBitMap = Vector Bit
+type BlockBitMap = V.Vector Bit
 
 -- | Bitmap for INodes
 -- 1 indicating that inode is used
 -- 0 indicating that inode is free
-type INodeBitMap = Vector Bit
+type INodeBitMap = V.Vector Bit
 
 -- | SuperBlock is a first block in whole file system
 -- which contains metadata information
@@ -38,34 +46,72 @@ data SuperBlock = SBlock
   , blockCount :: Word64
   , freeBlocks :: Word64
   , freeINodes :: Word64
-  }
-  deriving (Show, Eq)
+  } deriving (Show, Eq)
 
 -- | Type of file that INode "holds"
--- 'File' - actual file on disk
+-- 'File' - regular file on disk
 -- 'Directory' - directory
 -- 'Link' - symbolic link
 data FileType = File | Directory | Link deriving (Show, Eq, Enum)
+
+data FileStat = FS
+  { size     :: Int
+  , fileType :: FileType
+  } deriving (Show, Eq)
+
+
+----------------------------INDEX---------------------------------
+
+-- | Index to either Block or INode
+type Index a = Word64
+
+
+-----------------------INODE AND BLOCK----------------------------
 
 -- | INode is a structure that holds metadata about file
 data INode = INode
   { blockCount :: Word64
   , fileStat   :: FileStat
-  , blocks     :: [Word64]
-  }
-  deriving (Show, Eq)
+  , blocks     :: [Index Block]
+  } deriving (Show, Eq)
 
 -- | Block in memory, which is actually a vector of bytes with fixed size
-newtype Block = Block {unBlock :: Vector Word8} deriving (Show, Eq)
+newtype Block = Block { unBlock :: V.Vector Word8 } deriving (Show, Eq)
 
-data FileStat = FS
-  { size     :: Int
-  , fileType :: FileType
-  }
-  deriving (Show, Eq)
+-- | File descriptor, which contains list of block indices
+data FileDescriptor = FileDescriptor
+  { unID     :: Int
+  , unBlocks :: [Index Block]
+  } deriving (Show, Eq)
 
-data FSError
 
-data FileDescriptor
+---------------------------ERRORS---------------------------------
 
-data File
+data FSError = EEXIST | ENXIST | ENOSPC | ENOMEM deriving (Eq, Show)
+
+--------------------------FUNCTIONS-------------------------------
+
+-- | Reads list of inodes indices from blocks
+readDir :: [Index Block] -> [Index INode]
+readDir = undefined
+
+-- | Reads hardlink
+readLink :: [Index Block] -> Index INode
+readLink = undefined
+
+-- | Reads Blocks and returns the name of the file
+getName :: [Index Block] -> FilePath
+getName = undefined
+
+-- | Writes copies first vector in the beginning of the 2nd vector,
+-- which ought to be larger than the 1st one
+putName :: V.Vector a -> V.Vector a -> V.Vector a
+putName = undefined
+
+-- | Converts Vector Word8 to ByteString
+toByteString :: V.Vector Word8 -> B.ByteString
+toByteString = V.foldr (flip B.snoc) B.empty
+
+-- | Converts ByteString to Vector Word8
+toVectorWord8 :: B.ByteString -> V.Vector Word8
+toVectorWord8 b = foldr ((V.++) . V.singleton) V.empty (B.unpack b)
